3fa03eeb7f798c19cf6607db80e42258c4bd0220
NoName <vdm.volkov249@mail.ru>
2021-02-14 00:02:27 +0400

    14.02.2021 0:02

--- script.js
+++ script.js
@@ -1,7 +1,32 @@
-document.getElementById("formTask_0").addEventListener("submit",function(event){
-    event.preventDefault();
-    document.getElementsByName("User").forEach(function(input){
-        document.getElementById("usrName").textContent="Здравствуй, "+input.value;
+let dcmt = document;
+
+//Приветствие в блоке
+let printName = function () {
+    let Name = "";
+    if (localStorage.getItem("Uname") == null) {
+        Name += "гость";
+    } else {
+        Name += localStorage.getItem("Uname");
+    }
+    dcmt.getElementsByName("User").forEach(function (input) {
+        dcmt.getElementById("usrName").textContent = "Привет, " + Name;
     });
-    
+}
+
+let disclosure=function(task){
+    dcmt.getElementById(task).style.visibility="visible";
+}
+
+//Задание 0
+dcmt.getElementById("formTask_0").addEventListener("submit", function (event) {
+    event.preventDefault();
+    dcmt.getElementsByName("User").forEach(function (input) {
+        localStorage.setItem("Uname", input.value);
+    });
+    printName();
 });
+
+
+
+//функции при запуске сраницы
+printName();
